fa6677eca35e3bebec00ebd3efbb3eeca3ef0ee2
julio <kaisun.c@gmail.com>
2015-09-11 18:09:45 +0800

    20150911.v01

--- manga_scripts/cover_analysis.py
+++ manga_scripts/cover_analysis.py
@@ -10,7 +10,7 @@ from matplotlib import pyplot as plt
 import numpy as np
 import cv2
 import subprocess
-
+from collections import Counter
 ar_list = [
     [2.247, 1.858, 1.167, 1.082, 0.694, 0.784],     #air gear
     [2.495, 2.009, 1.284, 1.214, 0.725, 0.7969],    #abara
@@ -20,7 +20,7 @@ ar_list = [
 def getAR(asepect_ration):
     occurence = []
     for manga_ar in ar_list:
-        crop_type = manga_ar.index(min(game_ar, key=lambda x:abs(x-aspect_ratio))) 
+        crop_type = manga_ar.index(min(manga_ar, key=lambda x:abs(x-aspect_ratio))) 
         occurence.append(crop_type)
     
     #find most common cover type
@@ -33,15 +33,42 @@ def getAR(asepect_ration):
     manga_index = ar_temp.index(min(ar_temp, key=lambda x:abs(x-aspect_ratio)))
     return manga_index, most_common
 
+def findCreasePositions(edges):
+    im = cv2.cvtColor(edges, cv2.COLOR_GRAY2RGB)
+    im_rotate = np.swapaxes(im,0,1)
+    
+    x_axis = []
+    for x in im_rotate:
+        #x = im_rotate[0]
+        rsum = 0
+        for y in x:
+            rsum += y[1]
+        #print rsum/255
+        x_axis.append(rsum/255)
+
+    s = sorted(x_axis, reverse=True)
+    s = s[0:10] #get the four folds numbers
+    s = set(s)
+    
+    positions = []
+    for line in s:
+        #pos = x_axis.index(line) #find indices for the lines
+        pos = [i for i, x in enumerate(x_axis) if x == line]
+        for p in pos:
+            positions.append(p)
+          
+    return sorted(positions)
+
+
 path = r"d:/cover/"
 mangas = os.listdir(path)
 
 for manga in mangas:
-    manga = mangas[2]
-    manga_path = path + manga
-    images = os.listdir(manga_path)
+manga = mangas[18]
+manga_path = path + manga
+images = os.listdir(manga_path)
 
-image = images[1] #
+image = images[0] #
 
 in_path = manga_path + "/" + image
 out_path = r"d:\output.jpg"
@@ -65,42 +92,37 @@ os.path.isfile(out_path)
 im = cv2.imread(out_path)
 im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY )
 
-edges = cv2.Canny(im,0,50)
-#edges = cv2.Canny(im,50,200)
-imgplot = plt.imshow(edges, 'gray')# numpy array is reversed, use this function
+edges = []
+edges.append(cv2.Canny(im,0,50)) # finds low contrast edges
+edges.append(cv2.Canny(im,200, 400)) #finds highest contrast edges, good for finding empty space
+
+x_axis_values = findFeaturePositions(edges[1])
+
+plt.subplot(311)
+plt.imshow(edges[0], 'gray')
+plt.subplot(312)
+plt.imshow(edges[1], 'gray')
+plt.subplot(313)
+plt.plot(x_axis_values)
+
+plt.subplots_adjust(left=0.0, right=1.0, bottom=0.0, top=1.0)
+#plt.imshow(edges[3], 'gray')
+
+#imgplot = plt.imshow(edge[4, 'gray')
 plt.show()
 
-im = cv2.cvtColor(edges, cv2.COLOR_GRAY2RGB)
-im_rotate = np.swapaxes(im,0,1)
-
-x_axis = []
-for x in im_rotate:
-    #x = im_rotate[0]
-    rsum = 0
-    for y in x:
-        rsum += y[1]
-    #print rsum/255
-    x_axis.append(rsum/255)
+#returns highest 10 positions of creases
 
 
-s = sorted(x_axis, reverse=True)
-s = s[0:10] #get the four folds numbers
-s = set(s)
-s
-positions = []
-for line in s:
-    #pos = x_axis.index(line) #find indices for the lines
-    pos = [i for i, x in enumerate(x_axis) if x == line]
-    for p in pos:
-        positions.append(p)
-      
-print sorted(positions)
-
+positions = findCreasePositions(edges[0])
+feature_positions = findFeaturePositions(edges[2])
 #find probable locations of creases and calculate widths of elements
-
 
 fi_width = width * 0.2177
 full_width = width + fi_width
+
+#general location for the creases in pixels
+#this is going to be off for each manga...
 
 crease_bi = int(full_width * 0.8271)
 crease_b = int(full_width * 0.5181)
@@ -114,22 +136,37 @@ cover_b_width = crease_bi - crease_b
 insert_b_width = full_width - crease_bi
 
 #find the line within a range of 2 pixels
-
-def findline(line, r):
-    r_min = line - r
-    r_max = line + r
+# match line with positions generated above. 
+def findline(guessed_positions, r, detected_positions):
+    r_min = guessed_positions - r
+    r_max = guessed_positions + r
     for x in range(r_min, r_max):
-        if x in positions:
+        if x in detected_positions:
             return x
         else:
-            print "not here"
-r = 2            
-if cover_index == 1:
-    crease_b_fin = findline(crease_b, r)
-    crease_f_fin = findline(crease_f, r)
-    crease_fi_fin = findline(crease_fi, r)
+            pass
 
-    #if not found extrapolate
+r = 3
+if cover_index == 1:
+    crease_b_fin = findline(crease_b, r, positions)
+    crease_f_fin = findline(crease_f, r, positions)
+    crease_fi_fin = findline(crease_fi, r, positions)
+    
+    # if not found extrapolate using best method
+    # 1. retry canny with next set of min max
+    # 2. look at history or future of same manga
+    # 3. try similar manga
+    # 4. use others to calculate width
+    
+    if crease_fi_fin is None:
+        r = 20 # increase search width as features also have margins
+        crease_fi_fin = findline(crease_fi, r, feature_positions)
+        
+
+        
+
+    
+    
     
         
                
---
+++ manga_scripts/manga_cover_feature_detection.py
@@ -0,0 +1,83 @@
+def findFeaturePositions(imgarray):
+    #imgarray = edges[1]
+    im = cv2.cvtColor(imgarray, cv2.COLOR_GRAY2RGB)
+    im_rotate = np.swapaxes(im,0,1)
+    
+    # build an array where we add up the intensity of the y-axis
+    # normalize to percentage
+    # reduce to single digit
+    # reduce to terniary 0,1,2 where 0 is empty, 1 are features, 2 are creases
+    
+    x_axis = []
+    x_axis_values = []
+    for x in im_rotate:
+        rsum = 0
+        for y in x:
+            rsum += y[1]
+        x_axis_values.append(((rsum/255)*100)/height)
+        num = ((rsum/255)*100)/height
+        num = int(round(num, -1))
+        num = num/10
+        #x_axis.append(num)
+        if num > 0 and num <= 4:
+            num = 1
+        elif num >= 5: # 9 would be straight line, need to account for slightly skewed lines
+            num = 2
+        x_axis.append(num)
+    return x_axis_values
+    plt.plot(x_axis_values)
+    # go through x_axis and find chunks of 0, 1-5, 6-10
+    i = 0
+    blocks = []
+    blocks.append(0)
+    while i < len(x_axis):
+        while x_axis[i] == x_axis[i + 1]:
+            i = i + 1
+            if i + 1 == len(x_axis):
+                break
+        i = i + 1
+        blocks.append(i)
+    
+    i = 0
+    empty_blocks = []
+    empty_blocks.append(0)
+    while i < len(x_axis_values):
+        while x_axis_values[i] == x_axis_values[i + 1]:
+            i = i + 1
+        i = i + 1
+        empty_blocks.append(i)
+    blocks = empty_blocks
+
+       
+    block_length = []
+    for x in range(0, len(blocks)):
+        if x + 1 == len(blocks):
+            pass
+        else:
+            block_length.append(blocks[x+1] - blocks[x])
+    
+    big_blocks = [i for i, x in enumerate(block_length) if x > (width * 0.02)]
+
+    # cover should be the busiest and longest block
+    cover_block = max(block_length)
+    
+    margin = int(width * 0.01)
+    f = 0
+    for x in range(0, block_length.index(cover_block)):
+        f = f + block_length[x]
+    cover_fi_pos = f - margin
+    f + cover_block
+
+    # go through the blocks and find their positions
+    interesting = []
+    for x in big_blocks:
+        f = 1
+        for y in range(0, x + 1):
+            f = f + block_length[y]
+        interesting.append(f)
+    
+    # find creases
+    creases = [i for i, x in enumerate(x_axis) if x == 2]
+    return interesting
+
+feature_pos = findFeaturePositions(edges[2])
